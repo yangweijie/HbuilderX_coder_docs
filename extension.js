a927a31035e09c74adbb309ebe267ad2aa4fb63f
yangweijie <917647288@qq.com>
2020-07-06 15:02:57 +0800

    实现读取文件，右侧信息提示的展示

--- extension.js
+++ extension.js
@@ -1,8 +1,16 @@
 var hx = require("hbuilderx");
+var fs = require('fs');
+var path = require("path");
+
 //该方法将在插件激活的时候调用
 function activate(context) {
 	let disposable = hx.commands.registerCommand('extension.helloWorld', () => {
-		hx.window.showInformationMessage('你好，这是我的第一个插件扩展。');
+		var current_dir = __dirname;
+		let uri = path.join(__dirname, 'str.html');
+		console.log(uri);
+		var html = fs.readFileSync(uri, 'utf8');
+		console.log(html);
+		hx.window.showInformationMessage(html);
 	});
 	//订阅销毁钩子，插件禁用的时候，自动注销该command。
 	context.subscriptions.push(disposable);
